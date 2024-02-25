631e3073f2a08a080f1a978ac033730c88f2c156
David-Saperstein <54864170+David-Saperstein@users.noreply.github.com>
2024-02-24 21:22:42 -0500

    updates the status every second

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ function getMinutes(){
 function isOpen(minutes){
     return (minutes >= 960 || minutes < 45)
 }
-
-let open = isOpen(getMinutes());
-document.getElementById("status").innerHTML = open ? "Open" : "Closed";
-document.getElementById("status").style.color = open ? "green" : "#c42a22";
-
+function updateStatus(){
+    let open = isOpen(getMinutes());
+    document.getElementById("status").innerHTML = open ? "Open" : "Closed";
+    document.getElementById("status").style.color = open ? "green" : "#c42a22";
+}
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+updateStatus();
+setInterval(updateStatus, 1000);
